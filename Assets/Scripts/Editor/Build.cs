52f600155622ec24bc37afc657983c19f435f3a7
Garry Newman <garrynewman@gmail.com>
2017-07-18 13:09:34 +0100

    Cleanup

--- Assets/Scripts/Editor/Build.cs
+++ Assets/Scripts/Editor/Build.cs
@@ -10,32 +10,7 @@ public class Build
     {
         var levels = new[] { "Assets/Test.unity" };
 
-        BuildPipeline.BuildPlayer( levels, "Build/Win32/fpsw_w32.exe", BuildTarget.StandaloneWindows, BuildOptions.AllowDebugging );
-        BuildPipeline.BuildPlayer( levels, "Build/Win64/fpsw_w64.exe", BuildTarget.StandaloneWindows64, BuildOptions.AllowDebugging );
+        BuildPipeline.BuildPlayer( levels, "Build/Win32/fpsw_w32.exe", BuildTarget.StandaloneWindows, BuildOptions.AllowDebugging | BuildOptions.Development );
+        BuildPipeline.BuildPlayer( levels, "Build/Win64/fpsw_w64.exe", BuildTarget.StandaloneWindows64, BuildOptions.AllowDebugging | BuildOptions.Development );
     }
-
-    [PostProcessBuild(1)]
-    public static void OnPostProcessBuild(BuildTarget target, string pathToBuiltProject)
-    {
-        //
-        // Only steam
-        //
-        if ( !target.ToString().StartsWith( "Standalone" ) )
-            return;
-
-        //
-        // You only need a steam_appid.txt if you're launching outside of Steam
-        //
-        FileUtil.ReplaceFile("steam_appid.txt", System.IO.Path.GetDirectoryName(pathToBuiltProject) + "/steam_appid.txt");
-
-        //
-        // You only need these dlls if you're launching outside of Steam
-        //
-        if (target == BuildTarget.StandaloneWindows)
-            FileUtil.ReplaceFile("steam_api.dll", System.IO.Path.GetDirectoryName(pathToBuiltProject) + "/steam_api.dll");
-
-        if (target == BuildTarget.StandaloneWindows64)
-            FileUtil.ReplaceFile("steam_api64.dll", System.IO.Path.GetDirectoryName(pathToBuiltProject) + "/steam_api64.dll");
-    }
-
 }
--- Assets/Scripts/SteamTest.cs
+++ Assets/Scripts/SteamTest.cs
@@ -6,29 +6,33 @@ using Facepunch.Steamworks;
 
 public class SteamTest : MonoBehaviour
 {
-    public static Facepunch.Steamworks.Client SteamClient;
     private ServerList.Request serverRequest;
     private Leaderboard leaderBoard;
 
     void Start ()
 	{
         //
+        // Don't destroy this when loading new scenes
+        //
+        DontDestroyOnLoad( gameObject );
+
+        //
         // Configure for Unity
+        // This is VERY important - call this before 
         //
 	    Facepunch.Steamworks.Config.ForUnity( Application.platform.ToString() );
 
         //
         // Create the steam client using Rust's AppId
         //
-        SteamClient = new Client( 252490 );
+        new Facepunch.Steamworks.Client( 252490 );
 
         //
         // Make sure we started up okay
         //
-        if ( !SteamClient.IsValid )
+        if ( Client.Instance == null )
         {
-            SteamClient.Dispose();
-            SteamClient = null;
+            Debug.LogError( "Error starting Steam!" );
             return;
         }
 
@@ -36,34 +40,42 @@ public class SteamTest : MonoBehaviour
         // Request a list of servers
         //
 	    {
-	        serverRequest = SteamClient.ServerList.Internet();
+	        serverRequest = Client.Instance.ServerList.Internet();
 	    }
 
         //
         // Request a leaderboard
         //
 	    {
-            leaderBoard = SteamClient.GetLeaderboard( "TestLeaderboard", Client.LeaderboardSortMethod.Ascending, Client.LeaderboardDisplayType.Numeric );
+            leaderBoard = Client.Instance.GetLeaderboard( "TestLeaderboard", Client.LeaderboardSortMethod.Ascending, Client.LeaderboardDisplayType.Numeric );
         }
 	}
+
+    private void OnDestroy()
+    {
+        if ( Client.Instance != null )
+        {
+            Client.Instance.Dispose();
+        }
+    }
 
     void OnGUI()
     {
         GUILayout.BeginArea( new Rect( 16, 16, Screen.width - 32, Screen.height - 32 ) );
 
-        if ( SteamClient != null )
+        if ( Client.Instance != null )
         {
-            GUILayout.Label( "SteamId: " + SteamClient.SteamId );
-            GUILayout.Label( "Username: " + SteamClient.Username );
+            GUILayout.Label( "SteamId: " + Client.Instance.SteamId );
+            GUILayout.Label( "Username: " + Client.Instance.Username );
 
-            GUILayout.Label( "Friend Count: " + SteamClient.Friends.AllFriends.Count() );
-            GUILayout.Label( "Online Friend Count: " + SteamClient.Friends.AllFriends.Count( x => x.IsOnline ) );
+            GUILayout.Label( "Friend Count: " + Client.Instance.Friends.AllFriends.Count() );
+            GUILayout.Label( "Online Friend Count: " + Client.Instance.Friends.AllFriends.Count( x => x.IsOnline ) );
 
-            if ( SteamClient.Inventory.Definitions != null )
-                GUILayout.Label( "Item Definitions: " + SteamClient.Inventory.Definitions.Length );
+            if ( Client.Instance.Inventory.Definitions != null )
+                GUILayout.Label( "Item Definitions: " + Client.Instance.Inventory.Definitions.Length );
 
-            if ( SteamClient.Inventory.Items != null )
-                GUILayout.Label( "Item Count: " + SteamClient.Inventory.Items.Length );
+            if ( Client.Instance.Inventory.Items != null )
+                GUILayout.Label( "Item Count: " + Client.Instance.Inventory.Items.Length );
 
             if ( serverRequest != null )
             {
@@ -116,9 +128,9 @@ public class SteamTest : MonoBehaviour
 
     void Update()
     {
-        if ( SteamClient != null )
+        if ( Client.Instance != null )
         {
-            SteamClient.Update();
+            Client.Instance.Update();
         }
     }
 
